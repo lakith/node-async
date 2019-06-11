86767a2399d17d84a7bb64c77f4516dd5cdc2bf4
lakith <lakith1995@gmail.com>
2019-06-11 11:16:51 +0530

    wrap up

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Event Start up.");
+
+setTimeOut(()=>{
+	console.log("Wait 2 Seconds")
+},2000);
+
+setTimeOut(()=>{
+	console.log("Wait 0 Seconds")
+},0);
+
+console.log("Event End.");
